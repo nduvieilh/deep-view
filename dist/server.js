4f91743f938cabe401e13618bfc4d976bd58facb
Nicky Duvieilh <nickyduvieilh@gmail.com>
2018-12-29 22:36:28 -0600

    Setup nodemon and babel

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,7 @@
 "use strict";
 
+require("@babel/register");
+
 var express = require('express');
 
 var bodyParser = require('body-parser');
@@ -9,7 +11,7 @@ var port = 3000;
 app.use(bodyParser.json());
 app.post('/api/extract', function (req, res) {
   var body = req.body;
-  console.log(body); // Stringify body
+  console.log('asdf', body); // Stringify body
 
   var response = JSON.stringify(body);
   res.send(response);
@@ -17,17 +19,4 @@ app.post('/api/extract', function (req, res) {
 app.listen(port, function () {
   return console.log("Example app listening on port ".concat(port, "!"));
 });
-/*
-var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.post('/profile', upload.array(), function (req, res, next) {
-  console.log(req.body);
-  res.json(req.body);
-});
-*/
 //# sourceMappingURL=server.js.map
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("@babel/register");
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -8,25 +10,11 @@ app.use(bodyParser.json());
 app.post('/api/extract', (req, res) => {
     let body = req.body;
     
-    console.log(body);
+    console.log('adsf', body);
     // Stringify body
     let response = JSON.stringify(body);
 
-    res.send(response);
+    res.send(response); 
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-/*
-var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
-
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-app.post('/profile', upload.array(), function (req, res, next) {
-  console.log(req.body);
-  res.json(req.body);
-});
-*/
